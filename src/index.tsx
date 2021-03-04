9f2084f5370b71b56266dd30d827c256168b2f69
DimaZharikov <DimaZharikov@users.noreply.github.com>
2021-03-04 18:34:41 +0300

    add styles. Change logical process.

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,18 @@ import App from "./App";
 import React from "react";
 
 import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {store} from "./Redux/Store";
 
 
 
     ReactDOM.render(
         <React.StrictMode>
-            <BrowserRouter>
+            <HashRouter>
                 <Provider store={store} >
                     <App  />
                 </Provider>
-                </BrowserRouter>
+                </HashRouter>
         </React.StrictMode>,
 
         document.getElementById('root')
